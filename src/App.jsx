9d81e9c471d5830fae04db9ab0774154dc9785ce
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-24 12:19:29 +0530

    header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Header from "./Component/Header";
+import Home from "./Component/Home";
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
-        <Route />
+        <Route path="/" element={Home}/>
       </Routes>
     </Router>
   );
