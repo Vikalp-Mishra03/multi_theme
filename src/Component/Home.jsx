ecbcabfe232a2fb59d87f4b0408d28c442773d9f
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-24 16:19:26 +0530

    section 1 complete

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,10 +1,73 @@
 
-import React from 'react'
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+import { transform } from 'framer-motion';
+
+const headingOptions = {
+    pos: "absolute",
+    left: '50%',
+    top: '50%',
+    transform: "translate(-50%, -50%)",
+    textTransform: 'uppercase',
+    p: '4',
+    size: '4xl'
+}
 
 export default function Home() {
-  return (
-    <>
+    return (
+        <Box>
+            <MyCarousel />
 
-    </>
-  )
+            <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+                <Heading textTransform={'uppercase'} py={'2'} borderBottom={'2px solid'} w={'fit-content'} m={'auto'}>Services</Heading>
+            </Container>
+        </Box>
+    );
+
 }
+
+const MyCarousel = () => (
+    <Carousel
+        autoPlay
+        infiniteLoop
+        interval={1000}
+        showStatus={false}
+        showThumbs={false}
+        showArrows={false}
+    >
+        <Box w="full" h={'100vh'}>
+            <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+            <Heading bgColor={'blackAlpha,.600'} color={'white'} {...headingOptions}>
+                Watch The Future
+            </Heading>
+        </Box>
+        <Box w="full" h={'100vh'}>
+            <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+            <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+                Future is Gaming
+            </Heading>
+        </Box>
+
+        <Box w="full" h={'100vh'}>
+            <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+            <Heading bgColor={'whiteAlpha.600'} color={'black'}{...headingOptions} >
+                Gaming on Console
+            </Heading>
+        </Box>
+
+        <Box w="full" h={'100vh'}>
+            <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+            <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+                Night life is cool
+            </Heading>
+        </Box>
+    </Carousel>
+);
+
