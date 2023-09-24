583206627dd9f4ecf9f1505bf2f0f5cb1f5d7226
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-24 22:33:14 +0530

    log in section complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./Component/Home";
 import Footer from "./Component/Footer";
 import Video from "./Component/Video";
 import Upload from "./Component/Upload";
+import LogIn from "./Component/LogIn";
+import SignUp from "./Component/SignUp";
 function App() {
   return (
     <Router>
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/video" element={<Video />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <Footer />
     </Router>
