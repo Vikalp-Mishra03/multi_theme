f85e93374f88942f65c76fa33efa1c07a7491d67
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-24 17:03:19 +0530

    footer complete

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,9 +1,35 @@
+import { Box, Button, HStack, Heading, Input, Stack, VStack, Text } from '@chakra-ui/react'
 import React from 'react'
-
+import { AiOutlineSend } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 export default function Footer() {
-  return (
-    <div>
-        Footer
-    </div>
-  )
+    return (
+        <Box bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
+            <Stack direction={['column', 'row']}>
+                <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+                    <Heading size={'md'} textTransform={'uppercase'} textAlign={['center', 'left']}>
+                        Subscribe to get Updates
+                    </Heading>
+                    <HStack borderBottom={'2px solid white'} py={'2'}>
+                        <Input placeholder='Enter Your Email here...' border={'none'} borderRadius={'none'} outline={'none'} focusBorderColor='none'>
+
+                        </Input>
+                        <Button p={'0'} colorScheme='purple' variant={'ghost'} borderRadius={'0 20px 20px 0'}>
+                            <AiOutlineSend size={'20'} />
+                        </Button>
+                    </HStack>
+                </VStack>
+                <VStack w={'full'} borderLeft={['none', '1px solid white']} borderRight={['none', '1px solid white']}>
+                    <Heading textTransform={'uppercase'} textAlign={'center'}>video hub</Heading>
+                    <Text>@all right received</Text>
+                </VStack>
+                <VStack w={'full'}>
+                    <Heading size={'md'} textTransform={'uppercase'}>Social media</Heading>
+                    <Button variant={'link'} colorScheme='white'><a href="https://github.com/Vikalp-Mishra03" target='blank'>Github</a></Button>
+                    <Button variant={'link'} colorScheme='white'><a href="https://www.instagram.com/vikalp_003" target='blank'>Instagram</a></Button>
+                    <Button variant={'link'} colorScheme='white'><a href="https://www.linkedin.com/in/vikalp-mishra" target='blank'>Linkedin</a></Button>
+                </VStack>
+            </Stack>
+        </Box>
+    )
 }
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -8,7 +8,7 @@ import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
-import { transform } from 'framer-motion';
+// import { transform } from 'framer-motion';
 
 const headingOptions = {
     pos: "absolute",
